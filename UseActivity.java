aa530b72bb35c2e89c616694f10469476b54421b
승우 <113088025+seungwoohan12@users.noreply.github.com>
2023-07-06 03:14:44 +0900

    초기화면, 이용약관 동의, 회원가입 유형선택, 개인 회원가입_개인정보 수집
    일단 회원가입 개인-이름 부분까지 수정하고 업로드 합니다!

---
+++ UseActivity.java
@@ -0,0 +1,14 @@
+package com.example.precede1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_use);
+    }
+}
